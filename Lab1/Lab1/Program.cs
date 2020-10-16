4e05af409da314710a93a67c0bc102fb53397eda
ugolnikov <ssugol@yandex.ru>
2020-10-16 20:59:10 +0300

    ttt

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -8,8 +8,7 @@ namespace Lab1
         {
             IniParser p = new IniParser();
             p.ParseIniFile("file.ini");
-            int x = p.Get<int>("sdag", "sageg");
-            Console.WriteLine(x);
+            //Console.WriteLine(x);
             int y = p.Get<int>("COMMON", "DiskCachePath");
             Console.WriteLine(y);
             double z = p.Get<double>("ADC_DEV", "BufferLenSecons");
