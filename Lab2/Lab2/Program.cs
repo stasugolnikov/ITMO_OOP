f9f501efede6392a75cec447447506ea863d27c5
ugolnikov <ssugol@yandex.ru>
2020-10-18 02:21:26 +0300

    1

---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product bread = Product.Create(13,"bread");
+            Product cheese = Product.Create(14,"cheese");
+            Shop shop = new Shop(228,"magaz");
+            shop.DeliverProducts(bread, 30, 10);
+            shop.DeliverProducts(cheese, 300, 10);
+        }
+    }
+}
